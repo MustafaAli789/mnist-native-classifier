f80e24960ea1fc9887332957a738ad8534f63041
Mustafa Ali <alimustafa0987@gmail.com>
2020-12-25 00:21:41 -0500

    Added clear button and updated titel

--- client/mnist-classifier/src/App.js
+++ client/mnist-classifier/src/App.js
@@ -3,12 +3,19 @@ import Canvas from './Components/Canvas'
 import { Button } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear';
 import './App.css'
 import axios from 'axios'
 import createMixins from '@material-ui/core/styles/createMixins';
 
 function App() {
   let centerStyle = {display: 'flex', alignItems: 'center', justifyContent: 'center'}
+
+  const clearCanvas = () => {
+    let canvas = document.getElementById("canvas")
+    let ctx = canvas.getContext("2d")
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
 
   const classifyNum = () => {
 
@@ -37,34 +44,41 @@ function App() {
 
     }
     img.setAttribute("src", canvas.toDataURL())
-
-    // console.log(pixAlphaData)
-    // console.log(canvas.toDataURL())
-
   }
 
   return (
     <div>
       <Grid container>
         <Grid style={centerStyle} item xs={12}>
-          <h1>Title</h1>
+          <h1>MNIST Classifier</h1>
         </Grid>
       </Grid>
       <Grid container>
-        <Grid style={centerStyle} item xs={12}>
+        <Grid style={{...centerStyle, flexDirection: 'column'}} item xs={12}>
           <Canvas />
+          <div style={{flexDirection: 'row', width: '280px'}}>
+            <Button
+              style={{ width: '50%' }}
+              variant="contained"
+              color="primary"
+              size="small"
+              startIcon={<SearchIcon />}
+              onClick={classifyNum}
+            >
+              Classify
+            </Button>
+            <Button
+              style={{ width: '50%' }}
+              variant="contained"
+              color="secondary"
+              size="small"
+              startIcon={<ClearIcon />}
+              onClick={clearCanvas}
+            >
+              Clear
+            </Button>
+          </div>
           <img id="image" src="" style={{display: "none"}}></img>
-        </Grid>
-        <Grid style={{...centerStyle, marginTop: '1rem'}} item xs={12}>
-          <Button
-            variant="contained"
-            color="primary"
-            size="small"
-            startIcon={<SearchIcon />}
-            onClick={classifyNum}
-          >
-            Classify
-          </Button>
         </Grid>
       </Grid>
     </div>
